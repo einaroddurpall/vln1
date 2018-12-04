5599ea078a97a0ad23aff3584fadc5c3685061b5
kristjanari <kristjant18@ru.is>
2018-12-04 14:16:57 +0000

    Framhald af síðasta commiti(gleymdist að save-a)...

--- repositories/CarRepository.py
+++ repositories/CarRepository.py
@@ -7,9 +7,10 @@ class CarRepository:
         self.__cars = self.get_cars()
 
     def add_car(self, car):
-        """Bæta bíl í .csv skrá fyrir viðeigandi bílaflokk"""
+        """Bæta bíl í .csv skrá fyrir viðeigandi bílaflokk og í bílalista flokksins"""
         with open("./data/{}.csv".format(self.__name.lower()), "a") as cars_file:
             cars_file.write(car.__repr__() + '\n')
+        self.__cars.append(car)
 
     def get_cars(self):
         """Ná í alla bíla úr viðeigandi bílaflokk"""
