401779519a92e5544abf9aec744b01bbdf4256e6
Flatkaka <hilmarpall@hotmail.com>
2018-12-04 11:29:31 +0000

    changes made to person and customer, comments added

--- models/Person.py
+++ models/Person.py
@@ -1,8 +1,18 @@
+
+
 class Person:
+    """The person class takes in name and ssn 
+    and is a parent class of costumer, Boss, and  Employee"""
 
     def __init__ (self, name, ssn):
         self.__name = name
         self.__ssn = ssn
+
+    def get_name(self):
+        return self.__name
+
+    def get_ssn(self):
+        return self.__name
 
 
 
