72aa3d36c61ea224fe8ae6f575c8bd009dcf0f65
einaroddurpall <einaropr@gmail.com>
2018-12-04 13:55:15 +0000

    Byrjun á skrá bíl

---
+++ main.py
@@ -0,0 +1,7 @@
+from ui.CarRentalUi import CarRentalUi
+
+def main():
+    ui = CarRentalUi()
+    ui.main_menu()
+
+main()
--- ui/CarRentalUi.py
+++ ui/CarRentalUi.py
@@ -1,25 +1,65 @@
 from os import system,name
 from time import sleep
-# from services.CarService import CarService
+from services.CarService import CarService
 from models.Car import Car
 
-def print_header():
-    pass
+def print_header(prompt=""):
+    system('clear')
+    print("Heimasíða", end="")
+    print(prompt)
+    print("="*40)
+
+def get_action():
+    return input()
+
+def home_menu(prompt):
+    print_header(prompt)
+    print("1.  Bílar\n2.  Viðskiptavinir\n3.  Skoða/skrá pantanir")
+    return get_action()
+
+def car_menu(prompt):
+    print_header(prompt)
+    print("1.  Skoða bíl\n2.  Skrá nýjan bíl\n3.  Skoða lausa bíla\n4.  Skoða bíla í útleigu")
+    return get_action()
+
+def make_car(prompt):
+    print_header(prompt)
+    registration_num = input("Enter registration number: ")
+    car_type = input("Enter car type: ")
+    sub_type = input("Enter sub type: ")
+    transmission = input("Enter the transmission: ")
+    milage = input("Enter the milage: ")
+    is_rentable = True
+    history = ""
+    new_car = Car(registration_num, car_type, sub_type, transmission, milage, is_rentable, history)
+    return new_car
 
 class CarRentalUi:
 
     def __init__(self):
-        # self.CarService = CarService()
-        pass
+        self.__CarService = CarService()
 
     def main_menu(self):
         action = ""
+        prompt = ""
         while action != "q":
-            system('clear')
-            print("Heimasíða")
-            print("="*40)
-            print("1.\tBílar\n2.\tViðskiptavinir\n3.\tSkoða/skrá pantanir")
-            action = input().lower()
+            action = home_menu(prompt)
+            if action == "1":
+                prompt = " / Bílar"
+                action = car_menu(prompt)
+                if action == "1":
+                    prompt += " / Skoða bíl"
+                    pass
+                elif action == "2":
+                    prompt += " / Skrá nýjan bíl"
+                    new_car = make_car(prompt)
+                    self.__CarService.register_car(new_car)
+                elif action == "3":
+                    prompt += " / Skoða lausa bíla"
+                    pass
+                elif action == "4":
+                    prompt += " / Skoða bíla í útleigu"
+                    pass
 
 
 
@@ -30,6 +70,7 @@ def main():
     # sleep(5)
 
     # system('clear')
-    pass
+    ui = CarRentalUi()
+    ui.main_menu()
 
 main()
