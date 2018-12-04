1ad53c1a3ac647279e6fc55b511cb99f119e8684
kristjanari <kristjant18@ru.is>
2018-12-04 15:00:17 +0000

    New CustomerRepository file and repr and str functions added to customer

--- models/Customer.py
+++ models/Customer.py
@@ -1,16 +1,35 @@
 from Person import Person
 
 class Customer(Person):
-    """Customer class, is a supclass of the Person class
-    takes in name, ssn. email, gsm, card_info, history
-    and the name and ssn get's sent to person parent class"""
+    """Customer class, is a subclass of the Person class
+    takes in name, ssn, email, gsm, card_info, history
+    and the name and ssn are sent to the person parent class"""
 
-    def __init__ (self,name, ssn, email, gsm, card_info = "", history = ''):
+    def __init__ (self,name, ssn, email, gsm, card_info = "", history = ""):
         Person.__init__(self, name, ssn)
         self.__email = email
         self.__gsm = gsm
         self.__card_info = card_info
-        self.__history = history
+        
+        if history == "":
+            self.__history = "This car has no history of rental."
+        else: 
+            self.__history = history
+
+    def get_name(self):
+        return self.__name
+
+    def get_ssn(self):
+        return self.__ssn
+
+    def get_email(self):
+        return self.__email
+
+    def get_gsm(self):
+        return self.__gsm
+
+    def get_card_info(self):
+        return self.__card_info
     
     def update_cutomer_info (self, history):
         """function that creates a history or adds
@@ -20,3 +39,13 @@ class Customer(Person):
     def show_history(self):
         """Prints out the history"""
         return self.__history
+
+    def __repr__(self):
+        return "Customer('{}','{}','{}','{}','{}','{}')".format(
+            self.__name, self.__ssn, self.__email, self.__gsm, self.__card_info, self.__history
+        )
+
+    def __str__(self):
+        return "Customer name: {}, SSN: {}, email: {}, gsm: {}, credit card information: {}, history: {}".format(
+            self.__name, self.__ssn, self.__email, self.__gsm, self.__card_info, self.__history
+        )
---
+++ repositories/CustomerRepository.py
@@ -0,0 +1,21 @@
+from models.Customer import Customer
+
+class CustomerRepository:
+
+    def __init__(self):      
+        self.__customers = self.get_customers()
+
+    def add_customer(self, customer):
+        """Bæta viðskiptavin í .csv skrá og í viðskiptavinalista"""
+        with open("./data/customers.csv", "a") as customers_file:
+            customers_file.write(customer.__repr__() + '\n')
+        self.__customers.append(customer)
+
+    def get_customers(self):
+        """Ná í alla viðskiptavini"""
+        customers = []
+        with open("./data/customers.csv") as customers_file:
+            for row in customers_file.readlines():
+                customer = eval(row.strip())
+                customers.append(customer)
+        return customers
