c0c1d3e0aea0158e3dce89dcf0325c6b8f47a283
Flatkaka <hilmarpall@hotmail.com>
2018-12-05 13:21:44 +0000

    flott

--- ui/CarRentalUi.py
+++ ui/CarRentalUi.py
@@ -18,6 +18,7 @@ class CarRentalUi:
         self.__CustomerService = CustomerService()
     
     def draw_car(self):
+        print()
         print("\033[1;34;1m{:<31}==============".format(""))
         sleep(0.35)
         print("{:<28}=={:<16}==".format("",""))
