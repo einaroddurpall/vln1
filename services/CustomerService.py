1e4261116cbd4e824c503612619f2b8f1c776ad1
totason <jonkrona@gmail.com>
2018-12-06 13:02:29 +0000

    vohooo

--- services/CustomerService.py
+++ services/CustomerService.py
@@ -9,7 +9,7 @@ class CustomerService:
 
     def customer_register(self):
         new_customer = Customer()
-        new_customer = new_customer.make_customer(self.__customer_repo.get_customers_list())
+        new_customer.make_customer(self.__customer_repo.get_customers_list())
         self.__customer_repo.add_customer(new_customer)
 
     def get_customer_list(self):
