1f5f2495942a2ce2aa2e488334b63453dc92692e
Flatkaka <hilmarpall@hotmail.com>
2018-12-06 12:37:15 +0000

    textaskranar i lagI!

--- services/CarService.py
+++ services/CarService.py
@@ -34,6 +34,14 @@ class CarService:
                 all_cars_list.append(car)
         return all_cars_list
 
+    def make_all_cars_dict(self):
+        all_car_dict = {}
+        for car in self.__all_cars_list:
+            car_info_list = [car.get_registration_num(), car.get_sub_type(), car.get_milage(), car.get_transmission()]
+            all_car_dict[car.get_car_type()] = all_car_dict.get(car.get_car_type(), []) + [car_info_list]
+            print(all_car_dict)
+        return all_car_dict
+
     def get_all_cars_list(self):
         return self._all_cars_list
 
