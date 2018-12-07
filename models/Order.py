54d2040e5750fa79dbd443f7f27f0a22f43951ea
totason <jonkrona@gmail.com>
2018-12-07 09:52:36 +0000

    Breyttu order kerfinu í drasl, þetta var svakalegt, gæti verið einhver bugs vona ekki

--- models/Order.py
+++ models/Order.py
@@ -1,5 +1,13 @@
-class Order:
+from services.CustomerService import CustomerService
+from models.Car import make_car_type
+import string
+from datetime import datetime, timedelta, date
+from os import system
+from services.CarService import CarService
+from time import sleep
+from models.Customer import make_number
 
+class Order:
     def __init__(self, customer="", car="", date_list=[], insurance="", card_info="", order_name=""):
         self.__customer = customer
         self.__car = car
@@ -7,6 +15,8 @@ class Order:
         self.__insurance = insurance
         self.__card_info = card_info
         self.__order_name = order_name
+        self.__customer_service = CustomerService()
+        self.__car_service = CarService()
     
     def get_customer(self):
         return self.__customer
@@ -39,6 +49,98 @@ class Order:
         return "{};{};{};{};{};{};{}".format(
             str(self.get_order_name()),repr(self.get_customer()), repr(self.get_car()), repr(self.get_first_day()), repr(self.get_last_day()), self.get_insurance(), self.get_card_info()
         )
+    
+    
+    def change_info(self, step):
+        if step == "1":
+            valid_ssn = False
+            while valid_ssn is not True:
+                ssn = input("Kennitala viðskiptavinar: ")
+                self.__customer = self.__customer_service.check_ssn(ssn)
+                if self.__customer:
+                    valid_ssn = True
+                else:
+                    print("Kennitala ekki á skrá")
+        elif step == "2":
+            step2 = False
+            while step2 is not True:
+                car_type = make_car_type()
+                valid_date = False
+                while valid_date != True:
+                    try:
+                        date1 = make_date(input("Afhendingardagur (DD.MM.YYYY): "))
+                        date2 = make_date(input("Skiladagur (DD.MM.YYYY): "))
+                        self.__date_list = make_date_list(date1, date2)
+                        valid_date = True
+                    except: 
+                        print("Vinsamlegast sláðu inn gilda dagsetningu")
+                        
+                self.__car = self.rent_car(car_type, self.__date_list)
+                if self.__car:
+                    continue_q = input("Halda áfram? (y/n) ").lower()
+                    if continue_q == "y":
+                        step2 = True
+                    system('clear')
+                else:
+                    print("Enginn bíll laus með þessi skilyrði")
+                    sleep(2)
+                    system('clear')
+                    print("Heimasíða / Skoða eða skrá pantanir / Skrá pantanir")
+                    print("="*40)
+        elif step == "3":
+            step3 = False
+            while step3 is not True:
+                number = input("Veldu tryggingu:\n1.  Grunntrygging\n2.  Aukatrygging\n")
+                if number == "2":
+                    self.__insurance = "aukaleg trygging"
+                    step3 = True
+                else:
+                    self.__insurance = "skyldu trygging"
+                    step3 = True
+        elif step == "4":
+            self.__card_info = make_number(16, "Kortanúmer: ", "Ólöglegt kortanúmer, reyndu aftur.")
+                #"Order " + str(self.__order_num))
+                #self.__order_num += 1
+
+
+    def rent_car(self, car_type, date_list):
+        """ Þetta fall tekur á móti car_type og date_list, býr til carlist fyrir viðeigandi car_type og athugar hvort einhver
+            bíll í þessum carlist sé laus á dögunum í date_list """
+        if car_type.lower() == "fólksbíll":
+            car_type_list = self.__car_service._car_repo_sedan.get_carlist()
+        elif car_type.lower() == "fimm sæta jeppi":
+            car_type_list = self.__car_service._car_repo_five_seat_suv.get_carlist()
+        elif car_type.lower() == "smárúta":
+            car_type_list = self.__car_service._car_repo_minibus.get_carlist()
+        elif car_type.lower() == "sjö sæta jeppi":
+            car_type_list = self.__car_service._car_repo_seven_seat_suv.get_carlist()
+        elif car_type.lower() == "smábíll":
+            car_type_list = self.__car_service._car_repo_small_car.get_carlist()
+
+        date_repo = self.__car_service.get_date_repo()
+        date_dict = date_repo.get_date_dict()
+        for car in car_type_list:
+            if car.check_availability(date_list, date_dict, car_type_list):
+                return car
+        return None
+
+def make_date(a_date):
+    new_string = ""
+    for letter in a_date:
+        if letter in string.digits:
+            new_string += letter
+    day = new_string[:2]
+    month = new_string[2:4]
+    year = new_string[4:]
+    return date(int(year), int(month), int(day))
+
+def make_date_list(date1, date2):
+    date_list = []
+    date_to_list = date1
+    while date_to_list <= date2:
+        date_list.append(date_to_list)
+        date_to_list += timedelta(days=1)
+    return date_list
 
     # def update_info(self):
     # choice = ""
