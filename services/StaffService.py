661ace431ed27c717225db2632050f8cd4360040
huginngri <huginngri@gmail.com>
2018-12-10 11:26:07 +0000

    Vinna með Hilmari í staff

--- services/StaffService.py
+++ services/StaffService.py
@@ -17,3 +17,12 @@ class StaffService:
         new_staff = Staff()
         new_staff.make_staff(self.__staff_list)
         self.__staff_repo.add_staff(new_staff)
+    
+    def staff_delete(self, staff):
+        self.__staff_list.remove(staff)
+        self.__staff_repo.update_staff_list()
+
+    def staff_update_info(self, staff):
+        staff.staff_change_info(self.__staff_list)
+        self.__staff_repo.update_staff_list()
+        #Pæling með pantanir sem eru skráðar á gamla viðskiptavinin
