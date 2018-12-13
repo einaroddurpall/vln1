8f6fa307b3f05faa10b2327d27b6c1b1a67f25f6
Flatkaka <hilmarpall@hotmail.com>
2018-12-13 11:23:56 +0000

    hægt að skrá sig út eða loka kerfi

--- ui/CarRentalUi.py
+++ ui/CarRentalUi.py
@@ -50,28 +50,31 @@ class CarRentalUi:
     def main_menu(self):
         """ Main menu er loop sem hættir þegar q er sett inn. Á flestum input stöðum (fyrir utan þegar beðið er um dagsetningar eða 
         bílaflokk) er hægt að setja inn "t" til að fara tilbaka eða "h" til að fara aftur á þessa síðu. """
-        login = False
-        while not login:
-            username = input("Notandanafn: ")
-            password = input("Lykilorð: ")
-            #Athugar hvort notandi sé til og skilar því, notandanum og hvort hann sé admin
-            login, admin, self.__staff = self.__staff_service.check_login(username, password)
-            if login == False:
-                system('clear')
-                print("Innskráning mistókst.")
-        action = ""
+        action = ''
         while action != "q":
-            prompt = "Heimasíða"
-            print_header(prompt)
-            if admin:
-                action = input("1.  Bílar\n2.  Viðskiptavinir\n3.  Skoða eða skrá pantanir\n4.  Starfsmenn\nq.  Skrá út\n").lower()
-                if action == '4':
-                    self.__staffUI(self.__staff)
-            else:
-                action = input("1.  Bílar\n2.  Viðskiptavinir\n3.  Skoða eða skrá pantanir\nq.  Skrá út\n").lower()
-            if action == "1":
-                self.__carUI()
-            elif action == "2":
-                self.__customerUI()
-            elif action == "3":
-                self.__orderUI()
+            system("clear")
+            login = False
+            while not login:
+                username = input("Notandanafn: ")
+                password = input("Lykilorð: ")
+                #Athugar hvort notandi sé til og skilar því, notandanum og hvort hann sé admin
+                login, admin, self.__staff = self.__staff_service.check_login(username, password)
+                if login == False:
+                    system('clear')
+                    print("Innskráning mistókst.")
+            action = ""
+            while action != "s" and action != "q":
+                prompt = "Heimasíða"
+                print_header(prompt)
+                if admin:
+                    action = input("1.  Bílar\n2.  Viðskiptavinir\n3.  Skoða eða skrá pantanir\n4.  Starfsmenn\ns.  Skrá út\nq.  Loka kerfi\n").lower()
+                    if action == '4':
+                        self.__staffUI(self.__staff)
+                else:
+                    action = input("1.  Bílar\n2.  Viðskiptavinir\n3.  Skoða eða skrá pantanir\ns.  Skrá út\nq.  Loka kerfi\n").lower()
+                if action == "1":
+                    self.__carUI()
+                elif action == "2":
+                    self.__customerUI()
+                elif action == "3":
+                    self.__orderUI()
